a2e8ee3c4ea0d44f54b31adc7b22141fe0b57742
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-06-25 11:53:39 -0500

    feat(Adding carousel component)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CarouselComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
