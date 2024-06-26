ce67bd809d4199c2452975410fa6a0ccbd835f9f
LChumi <112025391+LChumi@users.noreply.github.com>
2024-06-25 19:44:07 -0500

    feat:(Adding footer)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,CarouselComponent],
+  imports: [NavbarComponent,CarouselComponent,FooterComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
