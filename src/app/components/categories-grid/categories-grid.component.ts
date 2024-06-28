c57745652ca9f869d1c76688bfc5b50ed13ffdc2
LChumi <112025391+LChumi@users.noreply.github.com>
2024-06-28 06:27:04 -0500

    feat:(adding categories grid)

---
+++ src/app/components/categories-grid/categories-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories-grid.component.html',
+  styles: ``
+})
+export class CategoriesGridComponent {
+
+}
