0160b660d902446bfbc77ebffca295a4ecac2a3e
LChumi <112025391+LChumi@users.noreply.github.com>
2024-06-27 21:14:50 -0500

    feat(Boxes whit icons)

---
+++ src/app/components/boxes-icons/boxes-icons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boxes-icons',
+  standalone: true,
+  imports: [],
+  templateUrl: './boxes-icons.component.html',
+  styles: ``
+})
+export class BoxesIconsComponent {
+  
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { BoxesIconsComponent } from '../../components/boxes-icons/boxes-icons.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,CarouselComponent,FooterComponent],
+  imports: [NavbarComponent,CarouselComponent,FooterComponent,BoxesIconsComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
