513a16015355cce5ddfbaacbb893dcf4374af588
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-17 19:14:06 -0500

    refactor: reformat code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,31 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import ProductsComponent from "@pages/products/products.component";
+import {SingleProductComponent} from "@pages/single-product/single-product.component";
 
 export const routes: Routes = [
-    {
-        path:'bunna',
-        children:[
-            {
-                path: 'home',
-                loadComponent: () => import('./pages/home/home.component')
-            },
-            {
-                path: 'about',
-                loadComponent: () => import('./pages/about/about.component')
-            },
-            {
-                path: 'products',
-                loadComponent: () => import('./pages/products/products.component')
-            },
-          {
-            path: 'productos/:categoryId',component: ProductsComponent
-          }
-        ]
-    },
-    {path:'', redirectTo:'bunna/home',pathMatch:'full'},
-    {path:'**', redirectTo:'bunna/home', pathMatch:'full'}
+  {
+    path: 'bunna',
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component')
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about.component')
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('./pages/products/products.component')
+      },
+      {
+        path: 'productos/:categoryId', component: ProductsComponent
+      },
+      {
+        path: 'producto/:productoId', component: SingleProductComponent
+      }
+    ]
+  },
+  {path: '', redirectTo: 'bunna/home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'bunna/home', pathMatch: 'full'}
 ];
---
+++ src/app/pages/single-product/single-product.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from "@shared/navbar/navbar.component";
+
+@Component({
+  selector: 'app-single-product',
+    imports: [
+        NavbarComponent
+    ],
+  templateUrl: './single-product.component.html',
+  styles: ``
+})
+export class SingleProductComponent {
+
+}
