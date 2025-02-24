437de99cd4e35decc5607f2af3d2ee12fe3c0d74
LChumi <112025391+LChumi@users.noreply.github.com>
2025-02-23 23:23:04 -0500

    feat: added functions to single product view in html and ts

--- src/app/pages/single-product/single-product.component.ts
+++ src/app/pages/single-product/single-product.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {NavbarComponent} from "@shared/navbar/navbar.component";
+import {DataService} from "@services/data/data.service";
+import {Products} from "@models/data/products";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-single-product',
@@ -9,6 +12,39 @@ import {NavbarComponent} from "@shared/navbar/navbar.component";
   templateUrl: './single-product.component.html',
   styles: ``
 })
-export class SingleProductComponent {
+export class SingleProductComponent  implements OnInit {
 
+  private dataService = inject(DataService);
+  private route = inject(ActivatedRoute)
+
+  productos: Products[] = [];
+  productoFiltrado: Products[] =[];
+  producto: Products = {} as Products
+  productoId: any
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.productoId = +params['productoId'];
+    });
+
+    if (this.productoId){
+      this.loadProductsById(this.productoId);
+    }
+  }
+
+  loadProductsByCategory(categoryId: number) {
+    this.dataService.getProductos().subscribe(data => {
+      console.log(data)
+      console.log(categoryId);
+      this.productoFiltrado = data.filter(producto => producto.categoria_id === categoryId);
+    })
+  }
+
+  loadProductsById(id: number) {
+    this.dataService.getProductos().subscribe(data => {
+      this.productos = data.filter(producto => producto.id === id);
+      this.producto = this.productos[0];
+      this.loadProductsByCategory(this.producto.categoria_id);
+    })
+  }
 }
