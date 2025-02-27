13d9bbd98c33785b2021e7c8dfb23ded33f58cf0
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-27 16:11:30 -0500

    styles changes in navbar stykes and routerLinkActivate

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,22 +1,38 @@
-import {Component} from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {NgClass, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgStyle,
+    NgClass
   ],
   templateUrl: './navbar.component.html',
   styles: [`
     .active-link {
       text-decoration: underline;
+      color: #c4b89b;
+      transition: color 0.3s ease, text-decoration 0.3s ease;
     }
+
+    a {
+      color: #333;
+      transition: color 0.3s ease;
+    }
+
+    a:hover {
+      color: #c4b89b;
+    }
+
   `]
 })
 export class NavbarComponent {
 
+  router = inject(Router)
   mobileMenu = false;
 
 }
