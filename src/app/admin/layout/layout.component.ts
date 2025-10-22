63f351270aefbca882ef7fbaabad16a3329aa1e1
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-10-22 18:32:52 -0500

    fix: changes in views and update tags un meta and schema to admin pages

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {Router, RouterOutlet} from "@angular/router";
 import {AdminNavbarComponent} from "../components/admin-navbar/admin-navbar.component";
 import {AdminSidebarComponent} from "../components/admin-sidebar/admin-sidebar.component";
+import {environment} from "@environments/environment";
+import {SchemaService} from "@services/seo/schema.service";
+import {MetaService} from "@services/seo/meta.service";
 
 @Component({
   selector: 'app-layout',
@@ -22,5 +25,36 @@ import {AdminSidebarComponent} from "../components/admin-sidebar/admin-sidebar.c
   styles: ``
 })
 export class LayoutComponent {
+
+  private domain = environment.domain;
+
+  constructor(
+    private router: Router,
+    private schemaService: SchemaService,
+    private seoService : MetaService
+  ) {
+    const currentUrl = `${this.domain}${this.router.url}`;
+
+    const title ='Pagina de administracion | Bunna Shop'
+    const description ='Vista de administracion y gestion de BunnaShop pagina privada logueo'
+
+    this.seoService.updateMetaTags({
+      title,
+      description,
+      canonicalUrl: currentUrl,
+      og: {
+        title,
+        description,
+        url: currentUrl,
+        image: `${this.domain}/images/logos/bunnaCirc.webp`
+      }
+    });
+
+    const schema = this.schemaService.generateContentPageSchema(
+      currentUrl,
+      'Pagina de administracion',
+      description);
+      this.schemaService.injectSchema(schema, 'WebPage')
+  }
 
 }
