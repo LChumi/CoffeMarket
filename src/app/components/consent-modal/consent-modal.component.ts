ae9babd833d520db254be13eaee3c72bd0eace19
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-10-24 17:01:21 -0500

    fix: changes in services and modal components

--- src/app/components/consent-modal/consent-modal.component.ts
+++ src/app/components/consent-modal/consent-modal.component.ts
@@ -20,7 +20,7 @@ export class ConsentModalComponent implements AfterViewInit{
   ngAfterViewInit() {
     setTimeout(() => {
       this.showModal = ! this.consentService.hasConsented();
-    }, 1000)
+    }, 2000)
   }
 
   accept() {
