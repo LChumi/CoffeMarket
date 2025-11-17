e2600a874034c79692a502e305ed01b3f154162d
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-11-17 13:12:54 -0500

    feat(cache): configure Redis with JSON serializer and custom TTLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   domain: `https://www.bunnashop.com`,
   apiUrl: `https://apis.cumpleanos.com.ec/bunnashop/`,
-  imagesUrl: `https://apis.cumpleanos.com.ec/assist/images/producto`,
+  imagesUrl: '/images' ,
   production: true
 };
