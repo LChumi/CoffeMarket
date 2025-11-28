df635c38efcad110415d86950320d63fc2d05a98
LChumi <112025391+LChumi@users.noreply.github.com>
2025-11-27 23:55:07 -0500

    fix: changes in components change ngOnInit to gAfterViewInit

--- src/app/components/pop-us/pop-us.component.ts
+++ src/app/components/pop-us/pop-us.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pop-us',
@@ -6,17 +6,24 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './pop-us.component.html',
   styles: ``
 })
-export class PopUsComponent implements OnInit {
+export class PopUsComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      const triggerModal = () => {
+        setTimeout(() => {
+          this.showModal = false;
+        }, 5000); // se cierra en 5 segundos
+        window.removeEventListener('scroll', triggerModal);
+        window.removeEventListener('click', triggerModal);
+      };
+
+      window.addEventListener('scroll', triggerModal);
+      window.addEventListener('click', triggerModal);
+    }
+  }
 
   titulo: string='';
   mensage:string='';
   showModal:boolean=true;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.showModal = false;
-    }, 5000); // se cierra en 5 segundos
-  }
-
-
 }
