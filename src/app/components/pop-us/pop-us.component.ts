a69050f34d343a5ee2e86d7df567ce3697c9c6b9
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-11-28 09:09:07 -0500

    fix: modal view

--- src/app/components/pop-us/pop-us.component.ts
+++ src/app/components/pop-us/pop-us.component.ts
@@ -10,7 +10,7 @@ export class PopUsComponent implements AfterViewInit {
 
   titulo: string='';
   mensage:string='';
-  showModal:boolean=true;
+  showModal:boolean=false;
 
   ngAfterViewInit(): void {
     if (typeof window !== 'undefined') {
