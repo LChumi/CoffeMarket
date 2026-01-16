0c03534fe7e0b82c7c8628572c563838aac7b7cc
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2026-01-16 18:58:33 -0500

    styles: change sin styles and update resolvers

--- src/app/core/resolvers/productos.resolver.ts
+++ src/app/core/resolvers/productos.resolver.ts
@@ -2,8 +2,11 @@ import {ResolveFn} from "@angular/router";
 import {Producto} from "@models/producto";
 import {inject} from "@angular/core";
 import {ProductoService} from "@services/producto.service";
+import {catchError, of} from "rxjs";
 
 export const productosResolver: ResolveFn<Producto[]> = (route, state) => {
   const productoService = inject(ProductoService);
-  return productoService.getAll();
+  return productoService.getAll().pipe(
+    catchError(() => of([]))
+  );
 };
